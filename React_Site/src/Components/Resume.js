b757588fb43903808b5676f0645986e2b9310ccc
Felix Fernandez <fj99.fernandez@gmail.com>
2024-10-05 23:37:47 -0400

    adding description2 to work2

--- React_Site/src/Components/Resume.js
+++ React_Site/src/Components/Resume.js
@@ -55,6 +55,9 @@ class Resume extends Component {
           {work.description && work.description[0].trim() !== "" && (
             <p className="off-white">{work.description}</p>
           )}
+          {work.description2 && work.description2[0].trim() !== "" && (
+            <p className="off-white">{work.description2}</p>
+          )}
           {work.link && work.link[0].trim() !== "" && (
             <a className="smoothscroll" href={work.link}>
               {work.link}
